bb84fed8d559242f753468dd37ad53e61fa0599a
RafaelNevesdeOliveira <rafaelneves652@gmail.com>
2022-08-15 02:24:12 -0300

    enable cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { router } from "./routes";
-
+import cors from "cors";
 const app = express();
 const port = process.env.PORT || 4003
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 app.listen(port, () => {
